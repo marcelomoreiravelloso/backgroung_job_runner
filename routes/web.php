46c8d4d972aa080e151ba16bf58b507feccae834
Chromosplanet <marcelo@chromosplanet.com.br>
2024-11-15 01:50:49 -0300

    feat: Creating new routes for [admin/jobs]

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BackgroundJobController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/jobs', [BackgroundJobController::class, 'index'])->name('admin.jobs.index');
+Route::post('/admin/jobs/{job}/retry', [BackgroundJobController::class, 'retry'])->name('admin.jobs.retry');
+
